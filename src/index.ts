076a8132350b37f7d68287ed54a18f096e2204fe
gabrielduarte <gmduarte96@gmail.com>
2023-05-05 18:13:24 -0300

    Feature: configured cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Add a list of allowed origins.
+const allowedOrigins = ["http://localhost:3000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
