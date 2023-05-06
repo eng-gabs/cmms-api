82be6d69e355bc3593b625738e92abc25fa76156
gabrielduarte <gmduarte96@gmail.com>
2023-05-06 11:32:34 -0300

    Feature: configured dotenv and mocha for testing

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,18 @@
-import mongoose from "mongoose";
+import mongoose, { connect, set } from "mongoose";
 
 async function main() {
   try {
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(
-      "mongodb+srv://gmduarte96:SdCMvjNnQPl8jBfy@cluster0.rejc6oo.mongodb.net/?retryWrites=true&w=majority"
-    );
-    console.log("MongoDB Conected");
-  } catch (error) {}
+    set("strictQuery", true);
+    // await mongoose.connect(
+    //   "mongodb+srv://gmduarte96:SdCMvjNnQPl8jBfy@cluster0.rejc6oo.mongodb.net/?retryWrites=true&w=majority"
+    // );
+    await connect(process.env.DBHOST!, {
+      dbName: process.env.DBNAME!,
+    });
+    console.log("MongoDB Conected", process.env.DBNAME!);
+  } catch (error) {
+    console.error("Error connecting DB");
+  }
 }
 
 export const connectDB = main;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./db/connection";
 import { router } from "./routes/router";
+import { config } from "dotenv";
+
+config({
+  path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : ".env",
+});
+
+console.log("process", process.env.NODE_ENV!);
 
 const PORT = process.env.PORT || 3000;
 const app = express();
